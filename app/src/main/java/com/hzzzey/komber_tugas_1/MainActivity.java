384ffddabcb150e63514c742d243e20d9281b221
csrausyan <csrausyan@gmail.com>
2020-10-11 13:00:22 +0700

    add SensorManager and deviceSensor

--- app/src/main/java/com/hzzzey/komber_tugas_1/MainActivity.java
+++ app/src/main/java/com/hzzzey/komber_tugas_1/MainActivity.java
@@ -2,13 +2,25 @@ package com.hzzzey.komber_tugas_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    SensorManager sensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> deviceSensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        Log.d("sensor",deviceSensors.toString());
     }
 }
