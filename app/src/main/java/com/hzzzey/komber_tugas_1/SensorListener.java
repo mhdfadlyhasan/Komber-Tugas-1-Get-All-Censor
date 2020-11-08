d46c9cb8560621b01b680553be1c2e53ef806f70
AbrahamWong <abrahamwongxxx@gmail.com>
2020-11-08 15:38:20 +0800

    [NOT TESTED] Create .csv to record the data

--- app/src/main/java/com/hzzzey/komber_tugas_1/SensorListener.java
+++ app/src/main/java/com/hzzzey/komber_tugas_1/SensorListener.java
@@ -1,35 +1,46 @@
 package com.hzzzey.komber_tugas_1;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.SystemClock;
 import android.os.Vibrator;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SensorListener extends AppCompatActivity implements SensorEventListener, View.OnClickListener {
     private Sensor mSensorLight;
     private Sensor mAcceloMeter;
-    int i=0;
+    int i=0, iter = 1;
 
     Boolean record = false, kill = false;
+    long timer;
     SensorManager mSensorManager;
     LineChart LineChartAccel;
     List<Entry> entriesX,entriesY,entriesZ;
@@ -38,16 +49,21 @@ public class SensorListener extends AppCompatActivity implements SensorEventList
     Button button, btnSave;
     public TextView mLightSensorValue, mAccelometerValue;
 
-    // disini aman ga ya, kalo dimasukin ke StartStopTracking ntar fast as fuck
     Thread t = new Thread(new Runnable() {
         @Override
         public void run() {
-            long timer = System.currentTimeMillis();
-
             while (!kill) {
-                // stop record data setiap 10 detik
+                // stop record data setelah 10 detik
                 if (System.currentTimeMillis() - timer < 10000) record = true;
-                else record = false;
+                else {
+                    record = false;
+                    if (!btnSave.isEnabled()) runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            btnSave.setEnabled(true);
+                        }
+                    });
+                }
             }
         }
     });
@@ -56,6 +72,13 @@ public class SensorListener extends AppCompatActivity implements SensorEventList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor_listener);
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (!checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                requestPermissions(new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1001);
+            }
+        }
+
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mSensorLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
         mAcceloMeter = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
@@ -69,6 +92,7 @@ public class SensorListener extends AppCompatActivity implements SensorEventList
         button.setText("start tracking");
         button.setOnClickListener(this);
         btnSave.setOnClickListener(this);
+        btnSave.setEnabled(false);
 
         vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         entriesX = new ArrayList<Entry>();
@@ -85,6 +109,19 @@ public class SensorListener extends AppCompatActivity implements SensorEventList
         LineData data = new LineData(dataSetX,dataSetY,dataSetZ);
         LineChartAccel.setData(data);
         LineChartAccel.invalidate();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 101 :
+                if (grantResults.length > 0 && grantResults[0]
+                        == PackageManager.PERMISSION_DENIED) {
+                    Toast.makeText(this, "Anda harus mengijinkan aplikasi menulis sebelum melanjutkan.", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
 
     @Override
@@ -132,9 +169,34 @@ public class SensorListener extends AppCompatActivity implements SensorEventList
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.buttonstartstop :
+                timer = System.currentTimeMillis();
                 t.start();
                 break;
             case R.id.btn_save_record:
+                t.interrupt();
+                if (isExternalStorageWritable() && btnSave.isEnabled() && checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    String csv = "", filename = "test" + String.valueOf(iter) + ".csv";
+                    File csvFile = new File(getExternalFilesDir(null), filename);
+
+                    for (int j = 0; j < entriesX.size() - 1; j++) {
+                        String data;
+                        data = entriesX.get(j).getY() + "," + entriesY.get(j).getY() + "," + entriesZ.get(j).getY() + "\n";
+                        csv = csv.concat(data);
+                    }
+
+                    Log.d("Save record", csv);
+                    try {
+                        FileOutputStream fos = new FileOutputStream(csvFile);
+                        fos.write(csv.getBytes());
+                        fos.close();
+
+                        Toast.makeText(this, "File saved as " + filename, Toast.LENGTH_SHORT).show();
+                        iter++;
+                        btnSave.setEnabled(false);
+                    } catch (IOException err) {
+                        err.printStackTrace();
+                    }
+                }
                 break;
             default:
                 break;
@@ -146,12 +208,17 @@ public class SensorListener extends AppCompatActivity implements SensorEventList
         super.onDestroy();
 
         // Isi supaya LineChart rilis mem yang dipake
-        t.interrupt();
-        kill = true;
+        try {
+            t.interrupt();
+            kill = true;
+            t.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void isExternalStorageWritable() {
-
+    private boolean isExternalStorageWritable() {
+        return Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState());
     }
 
     public void addValue(SensorEvent event){
@@ -167,5 +234,10 @@ public class SensorListener extends AppCompatActivity implements SensorEventList
         LineChartAccel.getLineData().notifyDataChanged();
         LineChartAccel.notifyDataSetChanged();
         LineChartAccel.invalidate();
+    }
+
+    private boolean checkPermission(String permission) {
+        int check = ContextCompat.checkSelfPermission(this, permission);
+        return (check == PackageManager.PERMISSION_GRANTED);
     }
 }
